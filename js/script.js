5bdb3aae7b5f1d23ec5cff56f6092d929b90cb07
Tonny <tonnyx1294@hotmail.com>
2016-01-27 00:36:24 -0500

    changed layout and added carousel slide, pdf, and projects

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var myApp = angular.module('app', []);
+
+myApp.controller('MainCtrl', function ($scope){
+
+// $(document).ready(function(){
+	$("#carousel").carousel({
+	    interval: 1 //changes the speed
+	})
+// });
+
+});
